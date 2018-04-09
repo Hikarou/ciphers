23c62e93ffea38ee10b5185d72420c80755be340
José Pinto <jose.ferropinto@epfl.ch>
2018-04-09 17:40:38 +0200

    UPDATE : Add comment header for functions

--- caesar/caesar.py
+++ caesar/caesar.py
@@ -7,14 +7,26 @@ class Caesar:
         self.key = key
         self.verbose = verbose
 
-    def _decipher_char(self, char):
+    def _decipher_char(self, char: chr) -> chr:
+        """
+        Helper for decipher one char, uses the key given in constructor
+        :rtype: chr
+        :param char: The char to decipher
+        :return: The character deciphered
+        """
         c = f.get_position(char)
         if c == -1:
             return ''
         m = chr(ord('A') + (c - self.key) % (ord('Z') - ord('A') + 1))
         return m
 
-    def decipher_string(self, string):
+    def decipher_string(self, string: str) -> str:
+        """
+        Decipher a string using the key given in constructor
+        :rtype: str
+        :param string: The string to decipher
+        :return: The deciphered string
+        """
         clear = ''
         for c in string:
             clear += self._decipher_char(c)
@@ -22,14 +34,26 @@ class Caesar:
             print(clear)
         return clear
 
-    def _cipher_char(self, char):
+    def _cipher_char(self, char: chr) -> str:
+        """
+        Helper for cipher one char, uses the key given in constructor
+        :rtype: chr
+        :param char: The char to cipher
+        :return: The char ciphered
+        """
         m = f.get_position(char)
         if m == -1:
             return ''
         c = chr(ord('A') + (m + self.key) % (ord('Z') - ord('A') + 1))
         return c
 
-    def cipher_string(self, string):
+    def cipher_string(self, string: str) -> str:
+        """
+        Cipher a string using the key given in constructor
+        :rtype: str
+        :param string: The string to cipher
+        :return: The ciphered string
+        """
         ciphered = ''
         for c in string:
             ciphered += self._cipher_char(c)
