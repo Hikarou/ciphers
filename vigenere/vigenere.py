26d6b303e4ecbf67769f88c1a541ad9c648013cd
José Pinto <jose.ferropinto@epfl.ch>
2018-04-09 17:49:55 +0200

    UPDATE : add signatures and type hints

--- vigenere/vigenere.py
+++ vigenere/vigenere.py
@@ -7,7 +7,14 @@ class Vigenere:
         self.key = key
         self.verbose = verbose
 
-    def _decipher_char(self, char, key_letter):
+    def _decipher_char(self, char: chr, key_letter: chr) -> chr:
+        """
+        Helper for decipher one char
+        :rtype: chr
+        :param char: The char to decipher
+        :param key_letter: The key to decipher with
+        :return: The deciphered char
+        """
         c = f.get_position(char)
         shift = f.get_position(key_letter)
         if c == -1:
@@ -15,7 +22,13 @@ class Vigenere:
         m = chr(ord('A') + (c - shift) % (ord('Z') - ord('A') + 1))
         return m
 
-    def decipher_string(self, string):
+    def decipher_string(self, string: str) -> str:
+        """
+        Decipher a string using the key given in constructor
+        :rtype: str
+        :param string: The string to decipher
+        :return: The deciphered string
+        """
         clear = ''
         key_length = len(self.key)
         for i, c in enumerate(string):
@@ -24,7 +37,14 @@ class Vigenere:
             print(clear)
         return clear
 
-    def cipher_char(self, char, key_letter):
+    def cipher_char(self, char: chr, key_letter: chr) -> chr:
+        """
+        Helper for cipher one char
+        :param char: The char to cipher
+        :rtype: chr
+        :param key_letter: The key to cipher with
+        :return: The ciphered char
+        """
         m = f.get_position(char)
         shift = f.get_position(key_letter)
         if m == -1:
@@ -32,7 +52,13 @@ class Vigenere:
         c = chr(ord('A') + (m + shift) % (ord('Z') - ord('A') + 1))
         return c
 
-    def cipher_string(self, string):
+    def cipher_string(self, string: str) -> str:
+        """
+        Cipher a string using the key given in constructor
+        :rtype: str
+        :param string: The string to cipher
+        :return: The ciphered string
+        """
         ciphered = ''
         key_length = len(self.key)
         for i, c in enumerate(string):
