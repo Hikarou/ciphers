770a5d841818472c1c3ffd0e50fe342b8947e83c
José Pinto <jose.ferropinto@epfl.ch>
2018-05-14 10:22:01 +0200

    UPDATE : Add signature and coms

--- ciphers_helper/functions.py
+++ ciphers_helper/functions.py
@@ -1,4 +1,9 @@
-def clean_char(c):
+def clean_char(c: chr) -> chr:
+    """
+    Will return the char in upper case or delete if not a char in [a-zA-Z]
+    :param c: The char to clean
+    :return: The cleaned char
+    """
     if ord(c) in range(ord('A'), ord('Z') + 1):
         return c
     elif ord(c) in range(ord('a'), ord('z') + 1):
@@ -8,7 +13,12 @@ def clean_char(c):
     return c
 
 
-def get_position(c):
+def get_position(c: chr) -> int:
+    """
+    Maps the position of the char as [0..25] -> [A-Z] ; -1 otherwise
+    :param c: The char to get the position from
+    :return: The position
+    """
     c = clean_char(c)
     if c == '':
         return -1
